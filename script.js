edb0c2fcb0e83dae6033295449625b540584ddc7
Yanis Blanchard <yanis.blanchard@gmail.com>
2025-06-18 11:43:10 +0200

    étape 2

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 function askNumber(){
     const input = prompt("Guess my number !")
-    const givenNumber = Number(input)
-    return givenNumber
+    const result = Number(input)
+    return result
     
 }
 
-let result = askNumber()
-console.log("Tu as saisi : " + result);
+
+// console.log("Tu as saisi : " + givenNumber);
+
+
+function didIwin(num){
+    if (num === 22) {
+        alert("Bravo ! Vous avez deviné le nombre");
+    } else if(num > 22) {
+        alert("Plus petit");
+    } else if(num < 22) {
+        alert("Plus grand");
+
+    }
+    
+}
+
+function gamePlay(){
+    let givenNumber = askNumber()
+    didIwin(givenNumber)
+}
+gamePlay()
